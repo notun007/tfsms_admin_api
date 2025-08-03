3429f882d88ed6b387fca93d776a3ffab559a6f8
notun007 <asadur.cse@gmail.com>
2025-08-03 14:23:49 +0600

    added

---
+++ Technofair.Data/Infrastructure/TFAdmin/IAdminUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Data.Infrastructure.TFAdmin
+{   
+    public interface IAdminUnitOfWork
+    {
+        void Commit();
+        Task<bool> CommitAsync();
+        Task<bool> CommitWithTransaction();
+    }
+}
