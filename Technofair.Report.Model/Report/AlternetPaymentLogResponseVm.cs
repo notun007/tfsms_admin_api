19c24d8f7b2189c6e54dd9945a7b4f60ed250812
notun007 <asadur.cse@gmail.com>
2025-08-03 17:34:56 +0600

    ADDED

---
+++ Technofair.Report.Model/Report/AlternetPaymentLogResponseVm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Report.Model.Report
+{
+    public class AlternetPaymentLogResponseVm
+    {
+        //new
+        public long Id { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string TransactionId { get; set; }
+        public string CustomerNo { get; set; }
+        public string SubscriberName { get; set; }
+        public string TrxStatus { get; set; }
+        public string DeviceNumber { get; set; }
+        public string PackageName { get; set; }
+        public Decimal FirstLevelCommission { get; set; }
+        public Decimal FirstLevelAmount { get; set; }
+        public Decimal SecondLevelCommission { get; set; }
+        public Decimal SecondLevelAmount { get; set; }
+        public Decimal ThirdLevelAmount { get; set; }
+        public int PaymentAmount { get; set; }
+        public string EmployeeName { get; set; }
+        public string CompanyName { get; set; }
+        public string LogoUrl { get; set; }
+        public byte[] Logo { get; set; }
+        public string CompanyAddress { get; set; }
+        public string PrintDate { get; set; }
+
+
+        //old
+        //public DateTime PaymentDate { get; set; }
+        //public string PackageName { get; set; }
+        //public string TransactionId { get; set; }
+        //public string TrxStatus { get; set; }        
+        //public string DeviceNumber { get; set; }
+        //public string SubscriberName { get; set; }
+        //public double CommissionAmount { get; set; }
+        //public double Amount { get; set; }       
+
+    }
+}
---
+++ Technofair.Report.Model/TFAdmin/TFAClientPaymentInvoiceViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Report.Model.TFAdmin
+{
+    //For Report Design:TFSMS.Admin.Model.ViewModel.TFAdmin
+    public class TFAClientPaymentInvoiceViewModel
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; } = "Client Payment Invoice";
+        public string InvoiceNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyContactNo { get; set; }
+        public string CompanyEmail { get; set; }
+        public string CompanyCustomerName { get; set; }
+        public string CompanyCustomerAddress { get; set; }
+        public string CompanyCustomerContactNo { get; set; }
+        public string CompanyCustomerEmail { get; set; }
+        public int CompanyCustomerId { get; set; }
+        public bool IsCollected { get; set; }
+        public string BillingMonth { get; set; }
+        public string PackageType { get; set; }
+        public string PackageName { get; set; }
+        public string Date { get; set; } = Convert.ToDateTime(DateTime.Now).ToString("dd-MM-yyyy");
+        public decimal? Rate { get; set; }
+        public decimal Amount { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TotalMonthlyBillAmount { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string BillGeneratorName { get; set; }
+        public string PaymentStatus { get; set; } = "";
+
+    }
+}
