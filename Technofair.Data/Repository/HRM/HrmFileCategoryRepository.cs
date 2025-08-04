d190f9bad338f8e3e71ea67fe091e406a79d7e3d
notun007 <asadur.cse@gmail.com>
2025-08-04 19:09:29 +0600

    added

---
+++ Technofair.Data/Repository/HRM/HrmFileCategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿
+using TFSMS.Admin.Data.Infrastructure;
+using TFSMS.Admin.Data.Infrastructure.TFAdmin;
+using TFSMS.Admin.Model.HRM;
+
+namespace TFSMS.Admin.Data.Repository.HRM
+{
+    public interface IHrmFileCategoryRepository : IRepository<HrmFileCategory>
+    {
+        List<HrmFileCategory> GetAll();
+        int AddEntity(HrmFileCategory obj);
+       
+    }
+    public class HrmFileCategoryRepository : AdminBaseRepository<HrmFileCategory>, IHrmFileCategoryRepository
+    {
+        public HrmFileCategoryRepository(IAdminDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+
+        }
+
+        public List<HrmFileCategory> GetAll()
+        {
+            return DataContext.HrmFileCategories.ToList();
+        }
+        public int AddEntity(HrmFileCategory obj)
+        {
+            int Id = 1;
+            HrmFileCategory last = DataContext.HrmFileCategories.OrderByDescending(x => x.Id).FirstOrDefault();
+
+            if (last != null)
+            {
+                Id = last.Id + 1;
+
+            }
+            obj.Id = Id;
+            base.Add(obj);
+            return Id;
+        }
+
+    }
+}
