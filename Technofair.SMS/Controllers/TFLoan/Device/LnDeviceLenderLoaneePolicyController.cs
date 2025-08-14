a81787f96b36ee5707d3aebd0cee4ce576360bc0
notun007 <asadur.cse@gmail.com>
2025-08-14 12:39:52 +0600

    enhancement

--- Technofair.SMS/Controllers/TFLoan/Device/LnDeviceLenderLoaneePolicyController.cs
+++ Technofair.SMS/Controllers/TFLoan/Device/LnDeviceLenderLoaneePolicyController.cs
@@ -14,6 +14,8 @@ using Technofair.Utiity.Http;
 using TFSMS.Admin.Service.TFLoan.Device;
 using TFSMS.Admin.Service.TFAdmin;
 using TFSMS.Admin.Data.Infrastructure.TFAdmin;
+using TFSMS.Admin.Model.Common;
+using TFSMS.Admin.Model.ViewModel.Subscription;
 
 
 namespace TFSMS.Admin.Controllers.TFLoan.Device
@@ -52,38 +54,76 @@ namespace TFSMS.Admin.Controllers.TFLoan.Device
         public async Task<Operation> SaveDeviceLenderLoaneePolicy([FromBody] LnDeviceLenderLoaneePolicyViewModel obj)
         {
             Operation objOperation = new Operation();
-            LnDeviceLenderLoaneePolicy objLnDeviceLenderLoaneePolicy = new LnDeviceLenderLoaneePolicy();
+                       
+            
+            LnDeviceLenderLoaneePolicy objPolicy = new LnDeviceLenderLoaneePolicy();
 
 
-            var objExit = service.GetById(obj.Id);
+            var objPolicyExit = service.GetById(obj.Id);
+                      
 
-            if (objExit == null)
+            if (objPolicyExit == null)
             {
-                objLnDeviceLenderLoaneePolicy.Id = obj.Id;
-                objLnDeviceLenderLoaneePolicy.LenderId = obj.LenderId;
-                objLnDeviceLenderLoaneePolicy.LoaneeId = obj.LoaneeId;
-                objLnDeviceLenderLoaneePolicy.MonthlyInstallmentAmount = obj.MonthlyInstallmentAmount;
-                objLnDeviceLenderLoaneePolicy.PerRechargeInstallmentAmount = obj.PerRechargeInstallmentAmount;
-                objLnDeviceLenderLoaneePolicy.IsActive = obj.IsActive;
-                objLnDeviceLenderLoaneePolicy.CreatedBy = obj.CreatedBy;
-                objLnDeviceLenderLoaneePolicy.CreatedDate = DateTime.Now;
-                objOperation = await service.Save(objLnDeviceLenderLoaneePolicy);
-                objOperation.Message = "Device Lender Loanee Policy Created Successfully.";
+                #region Save To SMS
+
+                LnDeviceLenderLoaneePolicyViewModel objPayload = new LnDeviceLenderLoaneePolicyViewModel();
+
+                objPayload.Id = obj.Id;
+                objPayload.LenderId = obj.LenderId;
+                objPayload.LoaneeId = obj.LoaneeId;           
+                objPayload.MonthlyInstallmentAmount = obj.MonthlyInstallmentAmount;
+                objPayload.PerRechargeInstallmentAmount = obj.PerRechargeInstallmentAmount;
+                objPayload.IsActive = obj.IsActive;
+                objPayload.CreatedBy = obj.CreatedBy;
+                objPayload.CreatedDate = DateTime.Now;
+
+                var objCompanyCustomer = serviceCompanyCustomer.GetById(obj.LoaneeId);
+
+                var smsApiBaseUrl = objCompanyCustomer.SmsApiBaseUrl;
+
+                var url = smsApiBaseUrl + "/api/LnDeviceLenderLoaneePolicy/SaveDeviceLenderLoaneePolicy";
+
+                objOperation = await Request<LnDeviceLenderLoaneePolicyViewModel, Operation>.Post(url, objPayload);
+                               
+
+                #endregion
             }
-            if (objExit != null)
+
+            if (objOperation.Success == true)
             {
-                objExit.MonthlyInstallmentAmount = obj.MonthlyInstallmentAmount;
-                objExit.PerRechargeInstallmentAmount = obj.PerRechargeInstallmentAmount;
-                objExit.IsActive = obj.IsActive;
-                objExit.ModifiedBy = obj.ModifiedBy;
-                objExit.ModifiedDate = DateTime.Now;
-                objOperation = service.Update(objExit);
-                objOperation.Message = "Device Lender Loanee Policy Updated Successfully.";
+
+                if (objPolicyExit == null)
+                {
+                    objPolicy.Id = obj.Id;
+                    objPolicy.LenderId = obj.LenderId;
+                    objPolicy.LoaneeId = obj.LoaneeId;
+                    objPolicy.MonthlyInstallmentAmount = obj.MonthlyInstallmentAmount;
+                    objPolicy.PerRechargeInstallmentAmount = obj.PerRechargeInstallmentAmount;
+                    objPolicy.IsActive = obj.IsActive;
+                    objPolicy.CreatedBy = obj.CreatedBy;
+                    objPolicy.CreatedDate = DateTime.Now;
+                    objOperation = await service.Save(objPolicy);
+                    objOperation.Message = "Loan Policy Created Successfully.";
+                }
+                if (objPolicyExit != null)
+                {
+                    objPolicyExit.MonthlyInstallmentAmount = obj.MonthlyInstallmentAmount;
+                    objPolicyExit.PerRechargeInstallmentAmount = obj.PerRechargeInstallmentAmount;
+                    objPolicyExit.IsActive = obj.IsActive;
+                    objPolicyExit.ModifiedBy = obj.ModifiedBy;
+                    objPolicyExit.ModifiedDate = DateTime.Now;
+                    objOperation = service.Update(objPolicyExit);
+                    objOperation.Message = "Loan Policy Updated Successfully.";
+                }
             }
+
+
+
+
             return objOperation;
         }
 
-        //Old: From SMS
+        //Old: From SMS --ata r lagba ne... remove hobe....
         [HttpPost("Save")]
         public async Task<Operation> Save([FromBody] LnDeviceLenderLoaneePolicyViewModel obj)
         {
