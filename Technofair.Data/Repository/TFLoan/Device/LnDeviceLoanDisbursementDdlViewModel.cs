df40bcb710dfe4dc74c0b91055fcc4d88ca4310e
notun007 <asadur.cse@gmail.com>
2025-08-21 10:24:32 +0600

    added

---
+++ Technofair.Data/Repository/TFLoan/Device/LnDeviceLoanDisbursementDdlViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Data.Repository.TFLoan.Device
+{
+    public class LnDeviceLoanDisbursementDdlViewModel
+    {
+        public Int64 LoanId { get; set; }
+        public string? LoanNo { get; set; }
+    }
+}
