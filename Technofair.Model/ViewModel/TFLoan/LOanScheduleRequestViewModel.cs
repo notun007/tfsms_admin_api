1546fd8eeb9d260a765d97980bee5664cfd840fa
notun007 <asadur.cse@gmail.com>
2025-08-24 19:03:20 +0600

    added

---
+++ Technofair.Model/ViewModel/TFLoan/LOanScheduleRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.ViewModel.TFLoan
+{
+    public class LOanScheduleRequestViewModel
+    {
+        public int LoaneeId {get;set;}
+        public Int64 loanId { get; set; }
+        public int createdBy { get; set; }
+       
+    }
+}
