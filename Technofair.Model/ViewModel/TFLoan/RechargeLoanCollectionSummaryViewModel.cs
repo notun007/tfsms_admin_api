4d924dbc9fe06b5c76be312c221e5ec36bfa86b6
notun007 <asadur.cse@gmail.com>
2025-08-26 18:44:36 +0600

    added

---
+++ Technofair.Model/ViewModel/TFLoan/RechargeLoanCollectionSummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.ViewModel.TFLoan
+{
+    public class RechargeLoanCollectionSummaryViewModel
+    {
+        public string LoanId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal PaymentCharge { get; set; }
+
+    }
+}
