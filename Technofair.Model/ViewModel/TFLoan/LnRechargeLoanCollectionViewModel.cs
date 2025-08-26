96dcb979833073c6ebab4eb88d843c77a966ae9f
notun007 <asadur.cse@gmail.com>
2025-08-26 21:20:56 +0600

    added

---
+++ Technofair.Model/ViewModel/TFLoan/LnRechargeLoanCollectionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFSMS.Admin.Model.Accounts;
+using TFSMS.Admin.Model.Common;
+using TFSMS.Admin.Model.TFLoan.Device;
+
+namespace Technofair.Model.ViewModel.TFLoan
+{
+    public class LnRechargeLoanCollectionViewModel
+    {
+     
+        public long Id { get; set; }
+        public long LoanId { get; set; }
+        public int LenderId { get; set; }
+        public int LoaneeId { get; set; }
+        public short AnFPaymentMethodId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal? PaymentChargePercent { get; set; }
+        public decimal? PaymentCharge { get; set; }
+        public string? Remarks { get; set; }
+        public DateTime CollectionDate { get; set; }
+        public string TransactionId { get; set; }
+        public bool IsCancel { get; set; }
+        public int? CancelBy { get; set; }
+        public DateTime? CancelDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+
+    }
+}
