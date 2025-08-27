e0b2281537d70e1aeaa171033159cdacd9fc8658
notun007 <asadur.cse@gmail.com>
2025-08-27 15:40:12 +0600

    added

---
+++ Technofair.Model/ViewModel/Bank/BnkAccountInfoViewModel.cs
@@ -0,0 +1,43 @@
+using Technofair.Model.Bank;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Technofair.Model.ViewModel.Bank
+{
+    public  class BnkAccountInfoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int BnkBranchId { get; set; }
+        public int? BnkBankId { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string AccountName { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string AccountNo { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public int CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public int? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+
+        public int BnkBankId { get; set; }
+        public string BankName { get; set; }
+        public string BranchName { get; set; }
+    }
+}
---
+++ Technofair.Model/ViewModel/Bank/BnkBranchViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.ViewModel.Bank
+{
+    public class BnkBranchViewModel
+    {
+        public int Id { get; set; }
+        public int BnkBankId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Remarks { get; set; }
+
+        public Nullable<bool> Status { get; set; }
+        public Nullable<int> CreatedBy { get; set; }
+        public Nullable<DateTime> CreatedDate { get; set; }
+        public Nullable<int> ModifiedBy { get; set; }
+        public Nullable<DateTime> ModifiedDate { get; set; }
+
+        public string BankName { get; set; }
+
+    }
+}
