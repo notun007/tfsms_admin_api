f0fcb25c5a0efe6a64f785baddd3d546dd6910da
notun007 <asadur.cse@gmail.com>
2025-08-27 18:13:06 +0600

    added

---
+++ Technofair.SMS/Controllers/Bank/BnkAccountInfoController .cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Technofair.Data.Repository.Bank;
+using Technofair.Model.Bank;
+using Technofair.Service.Bank;
+using TFSMS.Admin.Data.Infrastructure.TFAdmin;
+using TFSMS.Admin.Model.Common;
+
+
+namespace OneZeroERP.Web.Areas.Bank.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BnkAccountInfoController : ControllerBase
+    {
+
+        private IBnkAccountInfoService service;
+        public BnkAccountInfoController()
+        {
+            var dbfactory = new AdminDatabaseFactory();
+            service = new BnkAccountInfoService(new BnkAccountInfoRepository(dbfactory), new AdminUnitOfWork(dbfactory));
+        }
+
+        #region BankAccountController
+
+        [HttpPost("GetAll")]
+        public List<BnkAccountInfo> GetAll()
+        {
+            List<BnkAccountInfo> list = service.GetAll();
+            return list;
+        }
+
+        [HttpPost("GetAccountInfoByBranchId")]
+        public List<BnkAccountInfo> GetAccountInfoByBranchId(int bankId, int branchId)
+        {
+            List<BnkAccountInfo> list = service.GetAll().Where(x=> x.BnkBankId == bankId && x.BnkBranchId == branchId).ToList();
+            return list;
+        }
+        #endregion
+
+    }
+}
