04bd4d57b999000bf617eba005a0f010e9e14014
notun007 <asadur.cse@gmail.com>
2025-08-27 18:12:28 +0600

    added

---
+++ Technofair.SMS/Controllers/Bank/BnkBankController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Technofair.Data.Repository.Bank;
+using Technofair.Lib.Model;
+using Technofair.Model.Bank;
+using Technofair.Service.Bank;
+using TFSMS.Admin.Data.Infrastructure.TFAdmin;
+namespace OneZeroERP.Web.Areas.Bank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BnkBankController : ControllerBase
+    {       
+        private IBnkBankService service;
+      
+        public BnkBankController()
+        {
+            var dbfactory = new AdminDatabaseFactory();
+            service = new BnkBankService(new BnkBankRepository(dbfactory), new AdminUnitOfWork(dbfactory));
+        }
+
+        [HttpPost("GetAll")]
+        public List<BnkBank> GetAll()
+        {
+            List<BnkBank> list = service.GetAll();
+            return list;
+        }
+    }
+}
+
