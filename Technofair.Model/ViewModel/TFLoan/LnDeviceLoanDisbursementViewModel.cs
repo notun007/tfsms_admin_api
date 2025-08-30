3ce362be4fe38ae71081119fc30af7b3df05d239
notun007 <asadur.cse@gmail.com>
2025-08-30 11:11:16 +0600

    property changed

--- Technofair.Model/ViewModel/TFLoan/LnDeviceLoanDisbursementViewModel.cs
+++ Technofair.Model/ViewModel/TFLoan/LnDeviceLoanDisbursementViewModel.cs
@@ -16,29 +16,28 @@ namespace TFSMS.Admin.Model.ViewModel.TFLoan
         public int LoaneeId { get; set; }
         public Int16 NumberOfDevice { get; set; }
         public decimal Rate { get; set; }
-        //Commented On 27.07.2025
-        //public decimal Amount { get; set; }
 
         #region New Fields: 27.05.2025
         [Column(TypeName = "decimal(12, 2)")]
-        public decimal? TotalAmount { get; set; }
+        public decimal TotalAmount { get; set; }
         [Column(TypeName = "decimal(12, 2)")]
-        public decimal? PaymentAmountPerDevice { get; set; }
+        public decimal PaymentAmountPerDevice { get; set; }
         [Column(TypeName = "decimal(12, 2)")]
         public decimal? DueAmountPerDevice { get; set; }
         [Column(TypeName = "decimal(12, 2)")]
         public decimal? DownPaymentAmount { get; set; }
         [Column(TypeName = "decimal(12, 2)")]
         public decimal LoanAmount { get; set; } // Not nullable
-        public Int16? LnTenureId { get; set; }
+        public Int16 LnTenureId { get; set; }
         [Column(TypeName = "decimal(12, 2)")]
-        public decimal? MonthlyInstallmentAmount { get; set; }
+        public decimal MonthlyInstallmentAmount { get; set; }
+        public DateTime InstallmentStartDate { get; set; }
         #endregion
-        public bool? IsClosed { get; set; }
-        public DateTime? InstallmentStartDate { get; set; }
+        public bool IsClosed { get; set; }
+        
         public string? Remarks { get; set; }
-        public int? CreatedBy { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
         public int? ModifiedBy { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public string? LenderName {  get; set; }
