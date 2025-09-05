3750915e0ca91f76b9e97b9cdcf06c3acd9a2d39
notun007 <asadur.cse@gmail.com>
2025-09-05 20:48:47 +0600

    added

---
+++ Technofair.Model/ViewModel/TFLoan/RecoverScheduledLoanResponseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.ViewModel.TFLoan
+{
+    public class RecoverScheduledLoanResponseViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        [Column(TypeName = "decimal(12, 2)")]
+        public decimal RequestedCollectionAmount { get; set; }
+        [Column(TypeName = "decimal(12, 2)")]
+        public decimal ActualCollectionAmount { get; set; }
+
+    }
+}
