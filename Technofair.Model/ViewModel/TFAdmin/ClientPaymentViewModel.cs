94646416f83fda04e93921b0ea7c7a5c99494386
notun007 <asadur.cse@gmail.com>
2025-11-05 19:29:29 +0600

    added

---
+++ Technofair.Model/ViewModel/TFAdmin/ClientPaymentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.ViewModel.TFAdmin
+{
+    public class ClientPaymentViewModel
+    {
+        public DateTime? ExpireDate { get; set; }
+    }
+}
