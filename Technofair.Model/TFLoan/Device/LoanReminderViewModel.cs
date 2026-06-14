e89dc58b6f0387566769a2c150e55e9ef20e7e8f
Asadur Rahman <asadur.cse@gmail.com>
2026-06-14 20:19:44 +0600

    added

---
+++ Technofair.Model/TFLoan/Device/LoanReminderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.TFLoan.Device
+{
+    public class LoanReminderViewModel
+    {
+        public string Message { get; set; }
+    }
+}
