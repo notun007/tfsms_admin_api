d73dd70061eef6922e8cd45eff7b42035f9288f9
FARIDA <faridamojumder@gmail.com>
2026-09-05 12:19:36 +0600

    added

---
+++ Technofair.Model/Common/PingResponseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technofair.Model.Common
+{
+    public class PingResponseViewModel
+    {
+        public string DeviceName { get; set; }
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+
+        public string Status { get; set; }
+        public long RoundtripTime { get; set; }
+        public string Message { get; set; }
+    }
+}
